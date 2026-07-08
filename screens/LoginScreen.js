3d4b28d2e9c61271b6551bae7211494b857726f2
cvasco1 <cavasco@uce.edu.ec>
2026-07-08 12:39:12 -0500

    añadir useState

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
 
